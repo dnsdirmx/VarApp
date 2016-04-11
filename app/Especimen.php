06b46b66d289364a2c81d3417896e796f832f600
Héctor Villa <dnsdirmx@outlook.com>
2016-04-10 19:12:07 -0500

    Creado el modelo especimen

---
+++ app/Especimen.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Especimen extends Model
+{
+	protected $fillable = [
+        'estado','sexo'];
+
+	protected $table = 'especimenes';
+    public function reporte()
+    {
+        return $this->BelongsTo('App\Reporte');
+
+	}
+}
